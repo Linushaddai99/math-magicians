f5337581f58f19d397df512044a46dafd0c2bd6e
Linus Shaddai <54178270+Linushaddai99@users.noreply.github.com>
2022-12-01 16:27:50 +0100

    Refactor class-based components to functional components

--- src/components/button.js
+++ src/components/button.js
@@ -1,19 +1,12 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-class Button extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
-
-  render() {
-    const { content, classes, handleClick } = this.props;
-    return (
-      <button type="button" className={`${classes}`} onClick={handleClick}>{content}</button>
-    );
-  }
-}
+const Button = (props) => {
+  const { content, classes, handleClick } = props;
+  return (
+    <button type="button" className={`${classes}`} onClick={handleClick}>{content}</button>
+  );
+};
 
 Button.propTypes = {
   content: PropTypes.string.isRequired,
