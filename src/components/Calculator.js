19bc1c93e47c5f59ab95a78d28415c903cdc407d
Linus Shaddai <54178270+Linushaddai99@users.noreply.github.com>
2022-12-01 10:13:01 +0100

    Add event handler math logic

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,34 +1,56 @@
 import React from 'react';
+import Button from './button';
+import calculate from './logic.js/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {};
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
   }
 
   render() {
+    const { total, next, operation } = this.state;
+
+    const handleClick = (event) => {
+      const results = calculate({ total, next, operation }, event.target.innerHTML);
+
+      this.setState({
+        total: results.total,
+        next: results.next,
+        operation: results.operation,
+      });
+    };
+
     return (
       <div className="calculator-div">
-        <div className="result">0</div>
-        <button type="button">AC</button>
-        <button type="button">+/-</button>
-        <button type="button">%</button>
-        <button type="button" className="orange">÷</button>
-        <button type="button">7</button>
-        <button type="button">8</button>
-        <button type="button">9</button>
-        <button type="button" className="orange">x</button>
-        <button type="button">4</button>
-        <button type="button">5</button>
-        <button type="button">6</button>
-        <button type="button" className="orange">-</button>
-        <button type="button">1</button>
-        <button type="button">2</button>
-        <button type="button">3</button>
-        <button type="button" className="orange">+</button>
-        <button type="button" className="zero">0</button>
-        <button type="button">.</button>
-        <button type="button" className="orange">=</button>
+        <div className="result">
+          <span>{total}</span>
+          <span>{operation}</span>
+          <span>{next}</span>
+        </div>
+        <Button content="AC" handleClick={handleClick} />
+        <Button content="+/-" handleClick={handleClick} />
+        <Button content="%" handleClick={handleClick} />
+        <Button content="÷" classes="orange" handleClick={handleClick} />
+        <Button content="7" handleClick={handleClick} />
+        <Button content="8" handleClick={handleClick} />
+        <Button content="9" handleClick={handleClick} />
+        <Button content="x" classes="orange" handleClick={handleClick} />
+        <Button content="4" handleClick={handleClick} />
+        <Button content="5" handleClick={handleClick} />
+        <Button content="6" handleClick={handleClick} />
+        <Button content="-" classes="orange" handleClick={handleClick} />
+        <Button content="1" handleClick={handleClick} />
+        <Button content="2" handleClick={handleClick} />
+        <Button content="3" handleClick={handleClick} />
+        <Button content="+" classes="orange" handleClick={handleClick} />
+        <Button content="0" classes="zero" handleClick={handleClick} />
+        <Button content="." handleClick={handleClick} />
+        <Button content="=" classes="orange" handleClick={handleClick} />
       </div>
     );
   }
