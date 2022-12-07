fe35da376f9381bd73aad0f860a386d237609681
Linus Shaddai <54178270+Linushaddai99@users.noreply.github.com>
2022-12-07 13:10:04 +0100

    Fix Linters

--- src/components/Pages/CalculatorPage.js
+++ src/components/Pages/CalculatorPage.js
@@ -3,7 +3,7 @@ import Calculator from '../Calculator/Calculator';
 
 const CalculatorPage = () => (
   <div className="container" id="calc-page">
-    <h2>Lets Do some Maths</h2>
+    <h2>Lets Do some Maths!</h2>
     <Calculator />
   </div>
 );
